90e690c7fa676b5eeb687fae8dd75aa389d6c9b6
patrick-yp-yu <patrick.yp.yu@gmail.com>
2022-01-25 19:19:52 -0800

    update 605. Can Place Flowers

--- 0605_CanPlaceFlowers.py
+++ 0605_CanPlaceFlowers.py
@@ -1,13 +1,8 @@
-# 605. Can Place Flowers
-# Yuan-Peng Yu
-# 01/25/2022
-
-
 # Sol1. Find the max #flowers that can be placed, then compare with n 
 class Solution:
     
     # Sol1
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    def canPlaceFlowers1(self, flowerbed: List[int], n: int) -> bool:
         
         count = 0
         length = len(flowerbed)
@@ -20,8 +15,42 @@ class Solution:
         
         return count >= n
     # Runtime: 164 ms, faster than 87.33% of Python3
+    # Memory Usage: 14.5 MB, less than 61.69% of Python3 
     # Time: O(N)
     # Space: O(N)
+    ###-----------------------------------------------------
     
-
+    # Sol2. compare previout vs current
+    # prev curr
+    # 0    0   => can plant
+    # 0    1   => cannot plant due to planted already
+    # 1    0   => cannot plant due to neighbor
+    # 1    1   => previous planted flower need to be removed because curr ==1
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    
+        count = 0
+        prev = 0
+        
+        for curr in flowerbed:
+            if curr == 0:
+                if prev == 0: # 0, 0 => can plant 
+                    count += 1
+                    curr = 1
+                    prev = curr
+                else: # prev = 1
+                    # cannot plant due to neighbor
+                    prev = curr
+            
+            else: # curr ==1
+                if prev == 0:
+                    # cannot plant due to planted already
+                    prev = 1 # update prev = curr
+                else: # prev = 1
+                    count -= 1 #previous planted flower need to be removed because curr ==1 
+                    prev = 1
+        return count >= n
+    # Runtime: 173 ms, faster than 72.02% of Python3
+    # Memory Usage: 14.4 MB, less than 84.39% of Python3 
+    # Time: O(N)  traverse the list
+    # Space: O(1), only have fixed variables
     
